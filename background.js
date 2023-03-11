b67158214f39bd60bfef0ade058c1274f873f5b5
klaxonz <klaxon5296@gmail.com>
2023-03-11 22:06:14 +0800

    代码优化

--- background.js
+++ background.js
@@ -1,4 +1,85 @@
-chrome.runtime.onInstalled.addListener(() => {
-    
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    const { action } = request
+    if (action === 'login') {
+        // 登录
+        const { email, password } = request
+        fetch('http://localhost:9001/account/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }).then(response => {
+            if (!response.ok) {
+                return Promise.reject(response)
+            }
+            return response.json()
+        }).then(response => {
+            sendResponse({
+                code: "00000",
+                msg: "登录成功",
+                data: response
+            })
+        }).catch(error => {
+            if (error.statusCode === 401) {
+                sendResponse({
+                    code: "00001",
+                    msg: "重新登录",
+                    data: null
+                })
+            } else {
+                sendResponse({
+                    code: "90001",
+                    msg: `未知错误, 状态码：${error.statusCode}`,
+                    data: null
+                })
+            }
+        })
+    }
+    else if (action === "save") {
+        const { url } = request
+        fetch('http://localhost:9001/webpage/add', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                url: url
+            })
+        }).then(response => {
+            if (!response.ok) {
+                // 登录失败
+                return Promise.reject(response)
+            }
+            return response.json()
+        }).then(response => {
+            sendResponse({
+                code: "00000",
+                msg: "保存成功",
+                data: response
+            })
+        }).catch(error => {
+            if (error.statusCode === 401) {
+                sendResponse({
+                    code: "00001",
+                    msg: "重新登录",
+                    data: null
+                })
+            } else {
+                sendResponse({
+                    code: "90001",
+                    msg: `未知错误, 状态码：${error.statusCode}`,
+                    data: null
+                })
+            }
+        })
+    }
+    return true
 });
+
 
--- popup.js
+++ popup.js
@@ -1,30 +1,60 @@
-chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true
-}, function(tabs) {
-    var tab = tabs[0];
-    const data =  {
-        url: tab.url
-    }
-    fetch('http://localhost:9001/webpage/add', {
-        method: 'post',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then((resp => resp.json())).then((data => {
-        console.log(data)
-        if (data.code === '0000000') {
-            document.querySelector(".tips").innerText = "保存成功"
-        } else{
-            document.querySelector(".tips").innerText = data.desc
+// const url = window.location.href
+// chrome.runtime.sendMessage({url}, (response) => {
+//     console.log(response)
+// });
+
+
+// if add page success hide the login page
+// else show the login page to login
+
+document.addEventListener('DOMContentLoaded', () => {
+    addLoginClickEvent()
+})
+
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    chrome.runtime.sendMessage({
+        action: "save",
+        url: url
+    }, (response) => {
+        const code = response.code
+        if (code === '00000') {
+            document.getElementById('container1').style = 'display: none'
+            document.getElementById('container2').style = ''
+        } else {
+            document.getElementById('container1').style = ''
+            document.getElementById('container2').style = 'display: none'
         }
-    })).catch((error) => {
-        document.querySelector(".tips").innerText = "保存失败"
-    });
-});
+    })
+})
 
 
 
 
+function addLoginClickEvent() {
+    document.getElementById("login").addEventListener("click", () => {
+        const email = document.getElementById("email").value.trim()
+        const password = document.getElementById("password").value.trim()
+        if (!email) {
+            return
+        }
+        if (!password) {
+            return
+        }
+        chrome.runtime.sendMessage({
+            action: "login",
+            email: email,
+            password: password,
+        }, (response) => {
+            const code = response.code
+            if (code === '00000') {
+                document.getElementById('container1').style = 'display: none'
+                document.getElementById('container2').style = ''
+            } else {
+                document.getElementById('container1').style = ''
+                document.getElementById('container2').style = 'display: none'
+            }
+        })
+    })
+}
 
